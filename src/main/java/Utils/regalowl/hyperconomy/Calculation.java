98515693782ee8bf84ed51ae21f44a2dba6879b1
RegalOwl <I72@192.168.100.42>
2012-09-24 00:21:29 -0500

    minor cleanup

--- src/main/java/Utils/regalowl/hyperconomy/Calculation.java
+++ src/main/java/Utils/regalowl/hyperconomy/Calculation.java
@@ -92,22 +92,14 @@ public class Calculation {
 		SQLFunctions sf = hc.getSQLFunctions();
 		String playerecon = sf.getPlayerEconomy(p.getName());
 		try {
-			// Stores the total value for the item(s).
 			double totalvalue = 0;
-			// Gets the items config file.
 			int itemid = 0;
-			// Gets the item id and initializes the damage variable;
 			itemid = sf.getId(name, playerecon);
 			double damage = 0;
 			boolean isstatic = false;
 			isstatic = Boolean.parseBoolean(sf.getStatic(name, playerecon));
-			// Checks to make sure the price is not static.
 			if (!isstatic) {
-				// Gets the total damage of all items with the given id and
-				// divides it by the number of items.
 				damage = getDamage(itemid, amount, p);
-				// p.sendMessage("damage: " + damage);
-				// Gets the item's data from the items yaml file.
 				double shopstock = 0;
 				double value = 0;
 				double median = 0;
@@ -116,53 +108,35 @@ public class Calculation {
 				value = sf.getValue(name, playerecon);
 				median = sf.getMedian(name, playerecon);
 				icost = sf.getStartPrice(name, playerecon);
-				// Deactivates the initial pricing period if the initial price
-				// is greater than or equal to the hyperbolic price and if the
-				// shop has more than 0 items.
 				if (icost >= ((median * value) / shopstock) && shopstock > 0) {
 					sf.setInitiation(name, playerecon, "false");
 				}
-				// Sums the value of all desired items.
 				int counter = 0;
 				while (counter < amount) {
-					// Calculates the price for each individual item and sums
-					// them up in totalvalue.
 					double price = ((median * value) / shopstock);
 					price = applyCeilingFloor(name, playerecon, price);
 					shopstock = shopstock + 1;
 					totalvalue = totalvalue + price;
 					counter++;
 				}
-				// Factors in the total damage of all the chosen items.
 				totalvalue = totalvalue * damage;
-				// Checks to see if initiation is active. If it is it converts
-				// the totalvalue to the initial value.
 				Boolean initial = false;
 				initial = Boolean.parseBoolean(sf.getInitiation(name, playerecon));
 				if (initial == true) {
 					double ivalue = applyCeilingFloor(name, playerecon, icost);
 					totalvalue = ivalue * damage * amount;
 				}
-				// Checks if the price is infinite, and if it is sets the cost
-				// to a specific value that can later be identified.
 				if (totalvalue < Math.pow(10, 10)) {
-					// Rounds to two decimal places if not infinite
 					totalvalue = twoDecimals(totalvalue);
 				} else {
 					totalvalue = 3235624645000.7;
 				}
-				// If the item uses a static price, this calculates the correct
-				// value for it.
 			} else {
-				// Factors in the total damage.
 				damage = getDamage(itemid, amount, p);
-				// Gets the static price and multiplies it by the number of
-				// items.
 				double statprice = sf.getStaticPrice(name, playerecon);
 				double svalue = applyCeilingFloor(name, playerecon, statprice);
 				totalvalue = svalue * amount * damage;
 			}
-			// Returns the resulting value calculation.
 			return totalvalue;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -184,9 +158,7 @@ public class Calculation {
 		SQLFunctions sf = hc.getSQLFunctions();
 		try {
 			sf = hc.getSQLFunctions();
-			// Stores the total cost for the item(s).
 			double cost = 0;
-			// Checks to see if the price is static.
 			boolean isstatic = Boolean.parseBoolean(sf.getStatic(name, playerecon));
 			if (isstatic == false) {
 				double shopstock = 0;
@@ -197,11 +169,7 @@ public class Calculation {
 				oshopstock = shopstock;
 				value = sf.getValue(name, playerecon);
 				median = sf.getMedian(name, playerecon);
-				// Offsets the shop stock by one so that buying and selling will
-				// result in no money gained or lost. (The purchase price is set
-				// to what the price would have been after the purchase.)
 				shopstock = shopstock - 1;
-				// Sums up the cost of all items.
 				int counter = 0;
 				while (counter < amount) {
 					double price = ((median * value) / shopstock);
@@ -210,45 +178,28 @@ public class Calculation {
 					cost = cost + price;
 					counter++;
 				}
-				// Checks to see if initiation is active.
 				boolean initial = Boolean.parseBoolean(sf.getInitiation(name, playerecon));
 				if (initial == true) {
-					// If initiation is active, gets the correct cost.
 					double icost = 0;
 					icost = sf.getStartPrice(name, playerecon);
-					// If the shop has more than 0 items (which results in
-					// infinite values), it makes sure the real cost is less
-					// than the initial cost. If not, it turns off initiation.
 					if (cost < (icost * amount) && oshopstock > 0) {
 						sf.setInitiation(name, playerecon, "false");
-						// If the initial cost is indeed more than the normal
-						// cost, the cost is recalculated to the correct initial
-						// cost.
 					} else {
-						// Gets the initial tax multiplier and calculates the
-						// cost.
 						double price = applyCeilingFloor(name, playerecon, icost);
 						cost = price * amount;
 					}
 				}
-				// Checks if the cost is infinite, and if it is sets the cost to
-				// a specific value that can later be identified. If isn't
-				// infinite, it adds on the tax.
 				if (cost < Math.pow(10, 10)) {
-					// Rounds to two decimal places.
 					cost = twoDecimals(cost);
 				} else {
 					cost = 3235624645000.7;
 				}
-				// If the item is using a static price, this calculates the
-				// correct cost, factoring in the static tax.
 			} else {
 				double staticcost = sf.getStaticPrice(name, playerecon);
 				double price = applyCeilingFloor(name, playerecon, staticcost);
 				cost = price * amount;
 				cost = twoDecimals(cost);
 			}
-			// Returns the resulting cost calculation.
 			return cost;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -394,64 +345,30 @@ public class Calculation {
 	private double getDamage(int itemid, int amount, Player p) {
 		ETransaction ench = hc.getETransaction();
 		try {
-			// Holds the total damage for the specified item.
 			double damage = 0;
-			// Makes sure that the item can be damaged.
 			if (testId(itemid)) {
-				// Puts all of the specified item in the player's inventory into
-				// a HashMap.
 				Inventory pinv = p.getInventory();
 				HashMap<Integer, ? extends ItemStack> stacks = pinv.all(itemid);
-				// Gets the slot number of the inventory item being held by the
-				// player.
 				int heldslot = p.getInventory().getHeldItemSlot();
-				// Checks to see if the item in the player's hand is enchanted.
 				boolean hasenchants2 = ench.hasenchants(stacks.get(heldslot));
-				// Stores how many items have been processed. (An item stack can
-				// only have a quantity of 1 if it has durability.)
 				int totalitems = 0;
-				// Checks if the item a player is holding is the item being sold
-				// or bought and makes sure it is not enchanted.
 				if (p.getItemInHand().getTypeId() == itemid && hasenchants2 == false) {
-					// If the player is holding an eligible item for the
-					// transaction, it calculates the damage percent for the
-					// item and adds one item to the total number processed.
-					// This item is processed first because transactions favor
-					// the item in a player's hand over other items.
 					damage = getdurabilityPercent(stacks.get(heldslot));
 					totalitems++;
 				}
-				// Goes through each slot in a player's inventory and adds up
-				// the damage percentages until the total number of items
-				// processed equals the amount of items in the transaction.
 				int slot = 0;
 				while (slot < 36) {
-					// Checks to see if each item is enchanted.
 					boolean hasenchants = ench.hasenchants(stacks.get(slot));
-					// Makes sure the slot has an item, the item is not
-					// enchanted, the slot is not the slot the player is holding
-					// (as it was already processed), and that the total number
-					// of
-					// items in the transaction has not yet been reached.
 					if (stacks.get(slot) != null && totalitems < amount && slot != heldslot && hasenchants == false) {
-						// Calculates the damage percent for each item and sums
-						// them all up in damage.
 						damage = getdurabilityPercent(stacks.get(slot)) + damage;
 						totalitems++;
 					}
 					slot++;
 				}
-				// Divides the damage percentage by the number of items in the
-				// tranaction. This allows the damage multiplier to treat the
-				// entire transaction as one item with a certain
-				// durability.
 				damage = damage / amount;
-				// If an item cannot be damaged, it returns 1. (1 will have no
-				// effect on the cost/value calculation.)
 			} else {
 				damage = 1;
 			}
-			// Returns the calculated damage multiplier.
 			return damage;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -474,9 +391,6 @@ public class Calculation {
 	public double getdurabilityPercent(ItemStack i) {
 		try {
 			double durabilitypercent = 1;
-			// Attempts to get the durability. If an error is thrown this means
-			// that the item is not durable or something went wrong, and as a
-			// result of this it returns a durability of 1.
 			try {
 				double cdurability = i.getDurability();
 				double maxdurability = i.getData().getItemType().getMaxDurability();
@@ -484,7 +398,6 @@ public class Calculation {
 			} catch (Exception e) {
 				durabilitypercent = 1;
 			}
-			// Makes sure that the durability percent is not negative.
 			if (durabilitypercent < 0) {
 				durabilitypercent = 1;
 			}
@@ -562,14 +475,11 @@ public class Calculation {
 		try {
 			double cost = 0;
 			int counter = 0;
-			// Determines whether initial prices will be used.
 			Boolean initial = false;
 			initial = Boolean.parseBoolean(sf.getInitiation(name, playerecon));
-			// Checks if the price is set to static.
 			boolean isstatic = false;
 			isstatic = Boolean.parseBoolean(sf.getStatic(name, playerecon));
 			if (!isstatic) {
-				// Gets item data from the items yml file.
 				double shopstock = 0;
 				double value = 0;
 				double median = 0;
@@ -578,10 +488,7 @@ public class Calculation {
 				value = sf.getValue(name, playerecon);
 				median = sf.getMedian(name, playerecon);
 				icost = sf.getStartPrice(name, playerecon);
-				// Sums the cost in a loop.
 				while (counter < amount) {
-					// Calculates the price for each individual item and sums
-					// them up in the cost variable.
 					double price = ((median * value) / shopstock);
 					price = applyCeilingFloor(name, playerecon, price);
 					shopstock = shopstock + 1;
@@ -592,10 +499,7 @@ public class Calculation {
 					double price = applyCeilingFloor(name, playerecon, icost);
 					cost = price * amount;
 				}
-				// Checks if the price is infinite, and if it is sets the cost
-				// to a specific value that can later be used.
 				if (cost < Math.pow(10, 10)) {
-					// Rounds to two decimal places if not infinite
 					cost = twoDecimals(cost);
 				} else {
 					cost = 3235624645000.7;
@@ -606,7 +510,6 @@ public class Calculation {
 				cost = price * amount;
 			}
 			return cost;
-			// If there is some error this makes it easier to identify.
 		} catch (Exception e) {
 			e.printStackTrace();
 			Logger log = Logger.getLogger("Minecraft");
@@ -651,27 +554,22 @@ public class Calculation {
 		}
 	}
 
-	// Gets how many xp points are currently in the players xp bar.
 	public int getbarxpPoints(Player player) {
 		int lvl = player.getLevel();
 		int exppoints = (int) Math.floor(((3.5 * lvl) + 6.7) * player.getExp() + .5);
 		return exppoints;
 	}
 
-	// Gets how much xp is required to go from the current lvl with an empty xp
-	// bar to the next lvl.
 	public int getxpfornextLvl(int lvl) {
 		int exppoints = (int) Math.floor(((3.5 * lvl) + 6.7) + .5);
 		return exppoints;
 	}
 
-	// Gets the xp points at the minimum of the specified lvl.
 	public int getlvlxpPoints(int lvl) {
 		int exppoints = (int) Math.floor((1.75 * Math.pow(lvl, 2)) + (5 * lvl) + .5);
 		return exppoints;
 	}
 
-	// Gets how many xp points a player has total.
 	public int gettotalxpPoints(Player player) {
 		int lvl = player.getLevel();
 		int lvlxp = getlvlxpPoints(lvl);
@@ -680,11 +578,9 @@ public class Calculation {
 		return totalxp;
 	}
 
-	// Gets a players lvl from their total exp.
 	public int getlvlfromXP(int exp) {
 		double lvlraw = (Math.sqrt((exp * 7.0) + 25.0) - 5.0) * (2.0 / 7.0);
 		int lvl = (int) Math.floor(lvlraw);
-		// Incase math.floor fails.
 		if ((double) lvl > lvlraw) {
 			lvl = lvl - 1;
 		}
@@ -697,15 +593,12 @@ public class Calculation {
 		return (double) changedecimals / factor;
 	}
 
-	// Rounds to two decimal places.
 	public double twoDecimals(double input) {
 		int nodecimals = (int) Math.ceil((input * 100) - .5);
 		double twodecimals = (double) nodecimals / 100.0;
 		return twodecimals;
 	}
 
-	// Gets an item's true damage value, restoring damaged item's damage value
-	// to its original value and getting the correct damage value for potions.
 	public int getdamageValue(ItemStack item) {
 		int itd = item.getTypeId();
 		int da = getpotionDV(item);
@@ -724,18 +617,11 @@ public class Calculation {
 			double moneycap = hc.getYaml().getConfig().getDouble("config.dynamic-tax.money-cap");
 			double cbal = acc.getBalance(p.getName());
 			double maxtaxrate = hc.getYaml().getConfig().getDouble("config.dynamic-tax.max-tax-percent") / 100.0;
-			// Bukkit.broadcastMessage("Floor: " + moneyfloor);
-			// Bukkit.broadcastMessage("Cap: " + moneycap);
-			// Bukkit.broadcastMessage("Balance: " + cbal);
-			// Bukkit.broadcastMessage("Max Rate: " + maxtaxrate);
 			if (cbal >= moneycap) {
-				// Bukkit.broadcastMessage("Above Cap");
 				salestax = fprice * maxtaxrate;
 			} else if (cbal <= moneyfloor) {
-				// Bukkit.broadcastMessage("Below Floor");
 				salestax = 0;
 			} else {
-				// Bukkit.broadcastMessage("Dynamic Rate");
 				double taxrate = ((cbal - moneyfloor) / (moneycap - moneyfloor));
 				if (taxrate > maxtaxrate) {
 					taxrate = maxtaxrate;
@@ -743,7 +629,6 @@ public class Calculation {
 				salestax = fprice * taxrate;
 			}
 		} else {
-			// Bukkit.broadcastMessage("Dynamic Tax Disabled");
 			double salestaxpercent = hc.getYaml().getConfig().getDouble("config.sales-tax-percent");
 			salestax = (salestaxpercent / 100) * fprice;
 		}
