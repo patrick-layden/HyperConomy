35dd2560528c9684ee08365e26f140f9968a0d63
RegalOwl <I72@192.168.100.42>
2012-09-24 23:13:47 -0500

    Add copy parameter to setstaticall

--- src/main/java/Commands/regalowl/hyperconomy/Setstaticall.java
+++ src/main/java/Commands/regalowl/hyperconomy/Setstaticall.java
@@ -11,7 +11,6 @@ public class Setstaticall {
 		ArrayList<String> names = hc.getNames();
 		SQLFunctions sf = hc.getSQLFunctions();
 		InfoSign isign = hc.getInfoSign();
-		String name = "";
 		if (!(args.length == 1)) {
 			sender.sendMessage(ChatColor.DARK_RED + "Invalid parameters. Use /setstaticall ['true' or 'false']");
 			return;
@@ -21,15 +20,28 @@ public class Setstaticall {
 			setting = "true";
 		} else if (args[0].equalsIgnoreCase("false")) {
 			setting = "false";
+		} else if (args[0].equalsIgnoreCase("copy")) {
+			setting = "copy";
 		} else {
 			sender.sendMessage(ChatColor.DARK_RED + "Invalid parameters. Use /setstaticall ['true' or 'false']");
 			return;
 		}
 		new Backup();
-		for (int i = 0; i < names.size(); i++) {
-			name = names.get(i);
-			sf.setStatic(name, playerecon, setting);
+
+		if (setting.equalsIgnoreCase("copy")) {
+			for (int i = 0; i < names.size(); i++) {
+				String name = names.get(i);
+				sf.setStaticPrice(name, playerecon, sf.getStartPrice(name, playerecon));
+				sf.setStatic(name, playerecon, "true");
+			}
+			setting = "true + dynamic prices copied";
+		} else {
+			for (int i = 0; i < names.size(); i++) {
+				sf.setStatic(names.get(i), playerecon, setting);
+			}
 		}
+		
+		
 		isign.setrequestsignUpdate(true);
 		isign.checksignUpdate();
 		sender.sendMessage(ChatColor.GOLD + "All objects set to static pricing: " + setting + ".");
