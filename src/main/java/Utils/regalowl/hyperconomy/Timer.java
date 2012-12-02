8d231dd6fcd2463ed1aa177f1279c95461432600
RegalOwl <I72@192.168.100.39>
2012-12-01 22:38:28 -0600

    Add timer class.

---
+++ src/main/java/Utils/regalowl/hyperconomy/Timer.java
@@ -0,0 +1,37 @@
+package regalowl.hyperconomy;
+
+public class Timer {
+	
+	private int timerID;
+	private HyperConomy hc;
+	private long lastTime;
+	private long serverUptime;
+	private long frequency;
+	
+	Timer(long tickFrequency) {
+		frequency = tickFrequency;
+		lastTime = System.currentTimeMillis();
+		hc = HyperConomy.hc;
+		startTimer();
+	}
+	
+	
+	public void startTimer() {
+		timerID = hc.getServer().getScheduler().scheduleAsyncRepeatingTask(hc, new Runnable() {
+		    public void run() {
+		    	long time = System.currentTimeMillis();
+		    	serverUptime += (time - lastTime);
+		    	lastTime = time;
+		    }
+		}, frequency, frequency);
+	}
+	
+	public void stopTimer() {
+		hc.getServer().getScheduler().cancelTask(timerID);
+	}
+	
+	public long getServerUptime() {
+		return serverUptime;
+	}
+	
+}
