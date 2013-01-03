a0be2ebb8d843d5bda639d90894adbf9c94d5577
RegalOwl <regalowl@gmail.com>
2013-01-02 19:34:45 -0600

    Set version to .958

--- src/main/java/Utils/regalowl/hyperconomy/Transaction.java
+++ src/main/java/Utils/regalowl/hyperconomy/Transaction.java
@@ -1,14 +1,8 @@
 package regalowl.hyperconomy;
 
-import java.util.HashMap;
-import java.util.logging.Logger;
-
 import net.milkbowl.vault.economy.Economy;
-
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Effect;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
