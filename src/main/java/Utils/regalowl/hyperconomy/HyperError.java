4d626e9a88e80f63d42c9abe26f53d92e259d980
RegalOwl <regalowl@gmail.com>
2013-03-10 14:16:38 -0500

    Catch error handling errors.

--- src/main/java/Utils/regalowl/hyperconomy/HyperError.java
+++ src/main/java/Utils/regalowl/hyperconomy/HyperError.java
@@ -79,42 +79,44 @@ public class HyperError {
 	
 	@SuppressWarnings("deprecation")
 	private void handleError() {
-		boolean logError = hc.logErrors();
-		
-		
-		if (logError) {
-			hc.incrementErrorCount();
-			hc.getServer().getScheduler().scheduleAsyncDelayedTask(hc, new Runnable() {
-				public void run() {
-					FileTools ft = new FileTools();
-					String path = ft.getJarPath() + File.separator + "plugins" + File.separator + "HyperConomy" + File.separator + "errors";
-					ft.makeFolder(path);
-					path = path + File.separator + errornumber;
-					ft.makeFolder(path);
-					FileOutputStream fos;
-					try {
-						if (e != null) {
-							fos = new FileOutputStream(new File(path + File.separator + "stacktrace.txt"));
-							PrintStream ps = new PrintStream(fos);  
-							e.printStackTrace(ps); 
+		try {
+			boolean logError = hc.logErrors();
+			if (logError) {
+				hc.incrementErrorCount();
+				hc.getServer().getScheduler().scheduleAsyncDelayedTask(hc, new Runnable() {
+					public void run() {
+						FileTools ft = new FileTools();
+						String path = ft.getJarPath() + File.separator + "plugins" + File.separator + "HyperConomy" + File.separator + "errors";
+						ft.makeFolder(path);
+						path = path + File.separator + errornumber;
+						ft.makeFolder(path);
+						FileOutputStream fos;
+						try {
+							if (e != null) {
+								fos = new FileOutputStream(new File(path + File.separator + "stacktrace.txt"));
+								PrintStream ps = new PrintStream(fos);  
+								e.printStackTrace(ps); 
+							}
+						} catch (FileNotFoundException e1) {
+							e1.printStackTrace();
 						}
-					} catch (FileNotFoundException e1) {
-						e1.printStackTrace();
+						DataHandler sf = hc.getDataFunctions();
+						info = ft.getTimeStamp() + "\r\n"
+						+ "HyperConomy version: " + hc.getServerVersion() + "\r\n"
+						+ Bukkit.getName() + " version: " + Bukkit.getServer().getBukkitVersion() + "\r\n"
+						+ "UseMySQL='" + hc.useMySQL() + "'\r\nObjects Loaded='" + sf.objectsLoaded()
+						+ "'\r\n" + info;
+						ft.writeStringToFile(info, path + File.separator + "info.txt");
+						LanguageFile L = hc.getLanguageFile();
+						new ThreadSafeMessage(L.f(L.get("ERROR_HAS_OCCURRED"), errornumber), "hyperconomy.error", true);
 					}
-					DataHandler sf = hc.getDataFunctions();
-					info = ft.getTimeStamp() + "\r\n"
-					+ "HyperConomy version: " + hc.getServerVersion() + "\r\n"
-					+ Bukkit.getName() + " version: " + Bukkit.getServer().getBukkitVersion() + "\r\n"
-					+ "UseMySQL='" + hc.useMySQL() + "'\r\nObjects Loaded='" + sf.objectsLoaded()
-					+ "'\r\n" + info;
-					ft.writeStringToFile(info, path + File.separator + "info.txt");
-					LanguageFile L = hc.getLanguageFile();
-					new ThreadSafeMessage(L.f(L.get("ERROR_HAS_OCCURRED"), errornumber), "hyperconomy.error", true);
-				}
-			}, 0L);
-		} else {
-			LanguageFile L = hc.getLanguageFile();
-			new ThreadSafeMessage(L.f(L.get("ERROR_HAS_OCCURRED"), errornumber), "hyperconomy.error", true);
+				}, 0L);
+			} else {
+				LanguageFile L = hc.getLanguageFile();
+				new ThreadSafeMessage(L.f(L.get("ERROR_HAS_OCCURRED"), errornumber), "hyperconomy.error", true);
+			}
+		} catch (Exception e) {
+			//silence
 		}
 	}
 }
