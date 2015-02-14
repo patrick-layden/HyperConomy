0b1aaa915089a9dbc71068ea64d93e180d60e7f5
RegalOwl <regalowl@gmail.com>
2015-02-14 14:15:45 -0600

    Return more relevant upgrade versions if on older build.

--- src/main/java/regalowl/hyperconomy/util/UpdateChecker.java
+++ src/main/java/regalowl/hyperconomy/util/UpdateChecker.java
@@ -2,8 +2,10 @@ package regalowl.hyperconomy.util;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -22,6 +24,9 @@ public class UpdateChecker {
 	private boolean beta;
 	private boolean rb;
 	private boolean notifyInGame;
+	
+	private boolean upgradeAvailable = false;
+	private boolean runningDevBuild = false;
 	
 	public UpdateChecker(HyperConomy hc) {
 		this.hc = hc;
@@ -44,64 +49,100 @@ public class UpdateChecker {
 					conn.setReadTimeout(10000);
 					conn.addRequestProperty("User-Agent", "HyperConomy/v"+hc.getMC().getVersion()+" (by RegalOwl)");
 					conn.setDoOutput(true);
-					final BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-					final String response = reader.readLine();
-					final JSONArray array = (JSONArray) JSONValue.parse(response);
+					BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+					String response = reader.readLine();
+					JSONArray array = (JSONArray) JSONValue.parse(response);
 					if (array.size() == 0) return;
-					JSONObject lastObject = (JSONObject) array.get(array.size() - 1);
-					String nameData = (String) lastObject.get("name");
-					latestVersion = nameData.substring(1, nameData.indexOf(" "));
-					if (nameData.contains("[") && nameData.contains("]")) {
-						type = nameData.replace("[Lite]", "");
-						type = type.substring(type.indexOf("[") + 1, type.length());
-						type = type.substring(0, type.indexOf("]"));
+					boolean start = false;
+					ArrayList<String> acceptableUpgrades = new ArrayList<String>();
+					for (Object o:array) {
+						JSONObject jObject = (JSONObject)o;
+						String nameData = (String)jObject.get("name");
+						nameData = nameData.trim();
+						if (nameData.equals("v0.974.1 [Dev]")) start = true;
+						if (!start) continue;
+						String version = nameData.substring(1, nameData.indexOf(" "));
+						latestVersion = version;
+						String type = getType(nameData);
+						int code = getVersionComparisonCode(currentVersion, version);
+						//hc.getMC().logSevere("["+version+"]["+code+"]");
+						if (code >= 0) continue;
+						if (type.equalsIgnoreCase("BROKEN")) continue;
+						if (type.equalsIgnoreCase("DEV") && !dev) continue;
+						if (type.equalsIgnoreCase("BETA") && !beta) continue;
+						if (type.equalsIgnoreCase("RB") && !rb) continue;
+						acceptableUpgrades.add(nameData);
 					}
-					if (type.equalsIgnoreCase("DEV") && !dev) return;
-					if (type.equalsIgnoreCase("BETA") && !beta) return;
-					if (type.equalsIgnoreCase("RB") && !rb) return;
+					if (acceptableUpgrades.size() == 0) {
+						int code = getVersionComparisonCode(currentVersion, latestVersion);
+						if (code == 1) runningDevBuild = true;
+					} else {
+						latestVersion = acceptableUpgrades.get(acceptableUpgrades.size() - 1);
+						type = getType(latestVersion);
+						latestVersion = latestVersion.substring(1, latestVersion.indexOf(" "));
+						upgradeAvailable = true;
+					}
 					hc.getMC().runTask(new Runnable() {
 						public void run() {
-							int code = getVersionComparisonCode(currentVersion, latestVersion);
-							if (code == -1) {
-								if (notifyInGame) {
-									hc.getMC().runTaskLater(new Runnable() {
-										public void run() {
-											MessageBuilder mb = new MessageBuilder(hc, "NEW_VERSION_AVAILABLE");
-											mb.setValue(latestVersion);
-											mb.setType(" ["+type+"]");
-											for (HyperPlayer hp:hc.getMC().getOnlinePlayers()) {
-												if (hp.hasPermission("hyperconomy.admin")) {
-													hp.sendMessage(mb.build());
-												}
-											}
-										}
-									}, 600L);
-								}
+							if (upgradeAvailable) {
+								if (notifyInGame) notifyAdmins();
 								hc.getMC().logInfo("[HyperConomy]A new "+"["+type+"] build (" + latestVersion + ") is available for download.");
-							} else if (code == 0) {
-								hc.getMC().logInfo("[HyperConomy]No updates available.");
-							} else if (code == 1) {
+							} else if (runningDevBuild) {
 								hc.getMC().logInfo("[HyperConomy]No updates available. You are running a development build.");
+							} else {
+								hc.getMC().logInfo("[HyperConomy]No updates available.");
 							}
 						}
 					});
+				} catch (SocketTimeoutException te) {
+					hc.getMC().logInfo("[HyperConomy]Could not connect to server.");
 				} catch (Exception e) {
 					hc.gSDL().getErrorWriter().writeError(e);
 				}
 			}
 		}).start();
 	}
+	
+	private String getType(String data) {
+		String type = "";
+		if (data.contains("[") && data.contains("]")) {
+			type = data.replace("[Lite]", "");
+			type = type.substring(type.indexOf("[") + 1, type.length());
+			type = type.substring(0, type.indexOf("]"));
+		}
+		return type;
+	}
+	
+	private void notifyAdmins() {
+		hc.getMC().runTaskLater(new Runnable() {
+			public void run() {
+				MessageBuilder mb = new MessageBuilder(hc, "NEW_VERSION_AVAILABLE");
+				mb.setValue(latestVersion);
+				mb.setType(" ["+type+"]");
+				for (HyperPlayer hp:hc.getMC().getOnlinePlayers()) {
+					if (hp.hasPermission("hyperconomy.admin")) {
+						hp.sendMessage(mb.build());
+					}
+				}
+			}
+		}, 600L);
+	}
 
-	private int getVersionComparisonCode(String value, String referenceValue) {
-		String[] values = value.split("\\.");
-		String[] referenceValues = referenceValue.split("\\.");
-		if (Integer.parseInt(values[0]) > Integer.parseInt(referenceValues[0])) return 1;
-		if (Integer.parseInt(values[0]) < Integer.parseInt(referenceValues[0])) return -1;
-		if (Integer.parseInt(values[1]) > Integer.parseInt(referenceValues[1])) return 1;
-		if (Integer.parseInt(values[1]) < Integer.parseInt(referenceValues[1])) return -1;
-		if (Integer.parseInt(values[2]) > Integer.parseInt(referenceValues[2])) return 1;
-		if (Integer.parseInt(values[2]) < Integer.parseInt(referenceValues[2])) return -1;
-		return 0;
+	private int getVersionComparisonCode(String currentVersion, String latestVersion) {
+		try {
+			String[] values = currentVersion.split("\\.");
+			String[] referenceValues = latestVersion.split("\\.");
+			if (referenceValues.length < 3) return 1;
+			if (Integer.parseInt(values[0]) > Integer.parseInt(referenceValues[0])) return 1;
+			if (Integer.parseInt(values[0]) < Integer.parseInt(referenceValues[0])) return -1;
+			if (Integer.parseInt(values[1]) > Integer.parseInt(referenceValues[1])) return 1;
+			if (Integer.parseInt(values[1]) < Integer.parseInt(referenceValues[1])) return -1;
+			if (Integer.parseInt(values[2]) > Integer.parseInt(referenceValues[2])) return 1;
+			if (Integer.parseInt(values[2]) < Integer.parseInt(referenceValues[2])) return -1;
+			return 0;
+		} catch (Exception e) {
+			return -1;
+		}
 	}
 	
 	
